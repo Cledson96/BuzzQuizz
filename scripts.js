1330d918a18940a9a6b5143670c1005405596aba
cledson <cledson1996@gmail.com>
2022-07-01 20:37:01 -0300

    cledson

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function adicionarQuizz(ref) {
     for (let i = 0; i < TodosQuizz.length; i++) {
         let caixa = document.querySelector(".todos");
         caixa.innerHTML +=
-            `<div onclick="proximaPagina(this)" class="tema">
+            `<div onclick="proximaPagina(this)" class="tema id${i}">
                 <img src=${TodosQuizz[i].image}>
                 <p class="TituloQuizz">${TodosQuizz[i].title}</p>
             </div>`            

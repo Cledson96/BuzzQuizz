cc576ffeedc52f0be56274fc91cd36e633a57fd5
Anelize <anelizepelloso00@gmail.com>
2022-07-01 17:26:43 -0300

    Novo

--- script-anelize.js
+++ script-anelize.js
@@ -1,14 +1,37 @@
-/*let n;//variavel global que vai armazenar o indice i para que eu consiga identificar qual item é
+/*MINHAS VARIAVEIS GLOBAIS*/
+let divClicada;
+let n;
 function proximaPagina(elemento){
+    divClicada = elemento;
+    console.log(divClicada);    
     adicionandoDados();
-    irParaPagina2();
+    //irParaPagina2();
 }
 function irParaPagina2(){
-    //aqui vou acrescentar o display none
+    document.querySelector(".listaQuizz").classList.add("invisivel");
+    //preciso adicionar o displayflex apenas quando nao estiver usando com o display none
+    /*document.querySelector(".pagina2").classList.remove("invisivel");
+    document.querySelector(".pagina2").classList.add("adiciona");
+    document.querySelector(".tituloDoQuizz").classList.add("adiciona");
+    document.querySelector(".caixaDePergunta").classList.add("adiciona");
+    document.querySelector(".pergunta").classList.add("adiciona");
+    document.querySelector(".respostas").classList.add("adiciona");
+    document.querySelector(".emTexto").classList.add("adiciona");
+    document.querySelector(".porcentagemAcerto").classList.add("adiciona");
+    document.querySelector(".porcentagem").classList.add("adiciona");
+    document.querySelector(".porcentagemTexto").classList.add("adiciona");
+    document.querySelector(".porcentagemTexto img").classList.add("adiciona");
+    document.querySelector(".botaoReiniciar").classList.add("adiciona");
+    document.querySelector(".voltarHome").classList.add("adiciona");*/
 }
-function adicionandoDados(){
-  
-}*/
+function adicionandoDados(){ 
+    /*se a div for clicada,comparo ela com minha lista
+    eu retorno o indice
+    a partir dele eu armazeno as minhas informaçoes*/
+    n=0;
+    console.log(n);
+
+}
 
 //funcao que seleciona
 function selecionarAlternativa(elemento){
